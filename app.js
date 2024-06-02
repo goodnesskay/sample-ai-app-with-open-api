b4f21b5c1c602552f51e2a8de9bed01d36746e3f
Goodness Kayode <gtkbrain@gmail.com>
2024-06-02 15:42:39 +0000

    fix transcription

--- app.js
+++ app.js
@@ -35,13 +35,12 @@ app.post('/api/v1/chat', async (req, res) => {
 app.post('/api/v1/transcription/create', async (req, res) => {
     const userInput = req.body.audioFile;
     const dest = path.join(__dirname, fileDownloader.getFileNameFromUrl(userInput));
-    const audioFile = fileDownloader.downloadFile(userInput,dest);
+    // const audioFile = fileDownloader.downloadFile(userInput,dest);
     
     try {
-        res.json(dest);
         const response = await openai.audio.transcriptions.create({
             model: 'whisper-1',
-            file: fs.createReadStream(audioFile),
+            file: fs.createReadStream(userInput),
         });
         res.json({ message: response });
     } catch (error) {
